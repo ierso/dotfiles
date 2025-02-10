642f367f1b66963feb1f65b7795b67692dc5d47d
Cory Pierson <corypierson@mac.mynetworksettings.com>
2025-02-10 11:01:58 -0500

    update formatting to only format if prettier file exists

--- nvim/.config/nvim/lua/cory/plugins/formatting.lua
+++ nvim/.config/nvim/lua/cory/plugins/formatting.lua
@@ -18,11 +18,52 @@ return {
 				graphql = { "prettier" },
 				lua = { "stylua" },
 			},
-			format_on_save = {
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 1000,
-			},
+			format_on_save = function(bufnr)
+				-- Get the filetype of the current buffer
+				local ft = vim.bo[bufnr].filetype
+
+				-- Check if any formatter for this filetype is Prettier
+				local formatters = conform.get_formatters_by_ft(ft)
+				local has_prettier = false
+				for _, formatter in ipairs(formatters) do
+					if formatter.name == "prettier" then
+						has_prettier = true
+						break
+					end
+				end
+
+				if not has_prettier then
+					return false -- Don't format if not a Prettier filetype
+				end
+
+				-- List of Prettier configuration files
+				local root_files = {
+					".prettierrc",
+					".prettierrc.json",
+					".prettierrc.yml",
+					".prettierrc.yaml",
+					".prettierrc.json5",
+					".prettierrc.js",
+					".prettierrc.cjs",
+					"prettier.config.js",
+					"prettier.config.cjs",
+					".prettierrc.toml",
+				}
+
+				-- Check if any Prettier config exists in the project
+				local config_file = vim.fs.find(root_files, { upward = true })[1]
+
+				-- Enable formatting only if a config file is found
+				if config_file then
+					return {
+						lsp_fallback = true,
+						async = false,
+						timeout_ms = 1000,
+					}
+				else
+					return false -- No config found, don't format
+				end
+			end,
 		})
 
 		vim.keymap.set({ "n", "v" }, "<leader>mp", function()
